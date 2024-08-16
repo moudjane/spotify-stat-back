328016e841508c4b8087a2f9009da3acc9ffb5cf
moudjane <mathis.oudjane@gmail.com>
2024-08-16 20:57:17 +0200

    first commit

---
+++ src/main/java/com/moudjane/spotifystatback/SpotifyStatBackApplication.java
@@ -0,0 +1,13 @@
+package com.moudjane.spotifystatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyStatBackApplication.class, args);
+    }
+
+}
